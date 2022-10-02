df61228096af6647d521e4980bd046361824df87
tcdixon4 <36492457+tcdixon4@users.noreply.github.com>
2022-10-02 09:46:05 -0700

    [WP] in progress data extraction script; completed time domain and fft/power settings and all data; still need to add detector settings

--- notebooks/tutorial/extract_data.m
+++ notebooks/tutorial/extract_data.m
@@ -26,38 +26,78 @@ function [data, settings] = extract_data()
 
 %% Select a single folder containing .json files and process the session
 % data using Analysis-rcs-data
+data_dir = uigetdir;
 [unifiedDerivedTimes, timeDomainData, ~, ~, ~, ~, ~, PowerData, ~, ~, ~,...
     ~, ~, AdaptiveData, ~, ~, timeDomainSettings, powerSettings,...
     fftSettings, eventLogTable, metaData, stimSettingsOut, stimMetaData,...
     stimLogSettings, DetectorSettings, AdaptiveStimSettings, ...
-    AdaptiveEmbeddedRuns_StimSettings, ~] = ProcessRCS(uigetdir, 2);
+    AdaptiveEmbeddedRuns_StimSettings, ~] = ProcessRCS(data_dir, 2);
 dataStreams = {timeDomainData, PowerData, AdaptiveData};
 [combinedDataTable] = createCombinedTable(dataStreams, ...
                                           unifiedDerivedTimes, metaData);
 
 %% Create a simplified data table
 
+% timestamp
 timestamp = combinedDataTable.DerivedTime/1000;
 
-td = cell(1,4);
-for i = 1:4
-    td{i} = combinedDataTable(:,2+i);
-end
+% Time-Domain data
+td1 = combinedDataTable.TD_key0;
+td2 = combinedDataTable.TD_key1;
+td3 = combinedDataTable.TD_key2;
+td4 = combinedDataTable.TD_key3;
 
-pb = cell(1,8);
-for i = 1:4
-    td{i} = combinedDataTable(:,15+i);
-end
+% Power-Band data
+pb1 = combinedDataTable.Power_Band1;
+pb2 = combinedDataTable.Power_Band2;
+pb3 = combinedDataTable.Power_Band3;
+pb4 = combinedDataTable.Power_Band4;
+pb5 = combinedDataTable.Power_Band5;
+pb6 = combinedDataTable.Power_Band6;
+pb7 = combinedDataTable.Power_Band7;
+pb8 = combinedDataTable.Power_Band8;
 
+% LD outputs
 ld1 = combinedDataTable.Adaptive_Ld0_output;
 ld1 = correct_ld(ld1);
 ld2 = combinedDataTable.Adaptive_Ld1_output;
 ld2 = correct_ld(ld2);
 
+% state
 state = isolate_state_vector(combinedDataTable);
 
+% stim
+stim = isolate_stim_vector(combinedDataTable);
+
+% combine into a single data table and write to file
+data = table(timestamp, ...
+             td1, td2, td3, td4, ...
+             pb1, pb2, pb3, pb4, pb5, pb6, pb7, pb8, ...
+             ld1, ld2, state, stim);
+writetable(data, fullfile(data_dir, 'data.csv'))
 
 
+%% Create a settings file
+% If settings were changed in the middle of the selected session, this may
+% not work appropriately.
+fs_td = timeDomainSettings.samplingRate(1);
+fft_size = fftSettings.fftConfig.size;
+interval = fftSettings.fftConfig.interval;
+bit_shift = str2num(fftSettings.fftConfig.bandFormationConfig(6));
+
+band_edges_hz = '[';
+for i = 1:8
+    td_ch = num2str(ceil(i/2));
+    band = strcat('[', td_ch, ',[', ...
+                  num2str(powerSettings.powerBands(1).lowerBound(i)), ...
+                  ',', ...
+                  num2str(powerSettings.powerBands(1).upperBound(i)), ...
+                  ']],');
+    band_edges_hz = strcat(band_edges_hz, band);
+end
+band_edges_hz(end) = ']';
+
+% update_rate = 
 
 
 end
